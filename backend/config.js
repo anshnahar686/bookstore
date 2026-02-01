3928ae772a42c53bdd8598e5484c0778aa0baa6d
anshnahar686 <anshnahar686@gmail.com>
2026-02-01 23:22:52 +0530

    new changes

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,2 @@
 export const  PORT = 1000;
-export const mongoDBURL='mongodb+srv://anshnahar686_db_user:naharansh_com@cluster0.obzycy5.mongodb.net/?appName=Cluster0'
-   
+export const mongoDBURL="mongodb+srv://anshnahar686_db_user:naharansh_com@cluster0.obzycy5.mongodb.net/bookstore?retryWrites=true&w=majority"
